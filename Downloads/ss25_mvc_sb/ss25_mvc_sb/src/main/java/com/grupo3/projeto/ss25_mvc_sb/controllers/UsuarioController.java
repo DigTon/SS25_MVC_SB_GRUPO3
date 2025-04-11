be6cf983f7aa73d55198b61f099dac48441215d7
Diogo <gibertonidiogo88@gmail.com>
2025-04-11 19:10:10 -0300

    Adicionado script SQL para criação da tabela Usuario

--- Downloads/ss25_mvc_sb/ss25_mvc_sb/src/main/java/com/grupo3/projeto/ss25_mvc_sb/controllers/UsuarioController.java
+++ Downloads/ss25_mvc_sb/ss25_mvc_sb/src/main/java/com/grupo3/projeto/ss25_mvc_sb/controllers/UsuarioController.java
@@ -1,4 +1,26 @@
 package com.grupo3.projeto.ss25_mvc_sb.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.grupo3.projeto.ss25_mvc_sb.dao.UsuarioDAO;
+import com.grupo3.projeto.ss25_mvc_sb.models.Usuario;
+
+@RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
+    private UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    public UsuarioController() {}
+
+    @PostMapping
+    public void cadastrarUsuario(@RequestBody Usuario usuario) {
+        usuarioDAO.addUsuario(usuario.getUser(), usuario.getPassword());
+    }
+
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return usuarioDAO.listarUsuarios();
+    }
 }
