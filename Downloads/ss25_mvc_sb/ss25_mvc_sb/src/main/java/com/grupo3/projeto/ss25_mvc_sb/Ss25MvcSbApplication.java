7ebbb2ae9ffb82d7a5c1cd613eaca251b3c32a31
Diogo <gibertonidiogo88@gmail.com>
2025-04-10 21:14:51 -0300

    Projeto Spring Boot inicial criado com Spring Initializr

---
+++ Downloads/ss25_mvc_sb/ss25_mvc_sb/src/main/java/com/grupo3/projeto/ss25_mvc_sb/Ss25MvcSbApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.projeto.ss25_mvc_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss25MvcSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ss25MvcSbApplication.class, args);
+	}
+
+}
---
+++ Downloads/ss25_mvc_sb/ss25_mvc_sb/src/test/java/com/grupo3/projeto/ss25_mvc_sb/Ss25MvcSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.projeto.ss25_mvc_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss25MvcSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
